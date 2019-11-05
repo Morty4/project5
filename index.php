e0a771b401ac604cbcfc255b90afe54a60095b7c
Garth Tuck <gtuck@weber.edu>
2019-11-05 12:47:58 -0700

    index.php updated for no other reason than...I can!

--- index.php
+++ index.php
@@ -1,10 +1,7 @@
 <?php
 
-<<<<<<< HEAD
 echo "Great things to come...NB";
-=======
+echo "<br>";
 echo "Great things to come...maybe?";
-
+echo "<br>";
 echo "Narrator: Great things were not actually to come.";
-
->>>>>>> 5accdd33dfd65d52ae5a907e5a0b7c0f327708f6
