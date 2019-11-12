0c832e99660947a18a932c03280f71a5e3df126a
macchristensen <macchristensen@mail.weber.edu>
2019-11-12 09:35:32 -0700

    Create search.php

---
+++ search.php
@@ -0,0 +1,10 @@
+<?php include "admin/templates/header.php"; ?>
+   
+<div class="field">
+  <label class="label">Name</label>
+  <div class="control">
+    <input class="input" type="text" placeholder="Text input">
+  </div>
+</div>
+
+<?php include "admin/templates/footer.php"; ?>
